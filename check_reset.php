5d9b1409c75a982f33b3abc117912533cb1b38ff
yashik garg <yashik1227.cse18@chitkara.edu.in>
2021-06-09 00:16:07 +0530

    first commit

--- check_reset.php
+++ check_reset.php
@@ -26,8 +26,8 @@ if($num_row >= 1)
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'infothecafeteria@gmail.com';                 // SMTP username
-    $mail->Password = 'yashik123456';                           // SMTP password
+    $mail->Username = 'your_email';                 // SMTP username
+    $mail->Password = 'your_email_password';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
